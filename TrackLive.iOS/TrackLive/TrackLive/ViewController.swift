7b063a5b567043783c4ded395409c2f44dd84b2c
Alex Stefan <alex.steefan@gmail.com>
2018-08-31 10:27:57 +0300

    Add empty project for iOS

---
+++ TrackLive.iOS/TrackLive/TrackLive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrackLive
+//
+//  Created by Alex Stefan on 31/08/2018.
+//  Copyright © 2018 OrtecMobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
