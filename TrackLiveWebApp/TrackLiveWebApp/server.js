82b1953f81bf339b2307883e02b4ebb2f15efbe7
Alex Stefan <alex.steefan@gmail.com>
2018-09-27 22:19:37 +0300

    Remove files that were moved to separate repositories

--- TrackLiveWebApp/TrackLiveWebApp/server.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) { return res.send('Hello World!'); });
-app.listen(3000, function () { return console.log('Example app listening on port 3000!'); });
-//# sourceMappingURL=server.js.map
